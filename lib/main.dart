867eee89dbef0ad5044039a0ebaf9bb91373f9cc
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-14 18:24:17 -0400

    Manipular usuários, insert, update e delete.

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _HomeState extends State<Home> {
 
     Database db = await _recuperarBancoDados();
     //String sql = "SELECT * FROM usuarios WHERE idade = 28";
-    String sql = "SELECT * FROM usuarios WHERE idade = 40 OR idade = 28";
+    String sql = "SELECT * FROM usuarios";
     List usuarios = await db.rawQuery(sql);
 
     for(var usuario in usuarios){
@@ -69,11 +69,71 @@ class _HomeState extends State<Home> {
 
   }
 
+  _listarUsuarioPeloId(int id) async {
+
+    Database db = await _recuperarBancoDados();
+
+    List usuarios = await db.query(
+      "usuarios",
+      columns: ["id", "nome", "idade"],
+      where: "id = ?", //? é um caractere coringa
+      whereArgs: [id]
+    );
+
+    for(var usuario in usuarios){
+      print(
+          "item id: " + usuario["id"].toString() +
+              " nome: " + usuario["nome"] +
+              " idade " + usuario["idade"].toString()
+      );
+    }
+
+  }
+
+  _excluirUsuario(int id) async {
+
+    Database db = await _recuperarBancoDados();
+    
+    int retorno = await db.delete(
+      "usuarios",
+      where: "id = ?",
+      whereArgs: [id]
+    );
+
+    //print("Item qtde removida: $retorno");
+
+  }
+
+  _atualizarUsuario(int id) async {
+
+    Database db = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome" : "Qhuareen Paz Aubukerkeh ALTERADA",
+      "idade" : 30
+    };
+
+    int retorno = await db.update(
+      "usuarios",
+      dadosUsuario,
+      where: "id = ?",
+      whereArgs: [id]
+    );
+
+    print("Item qtde atualizada: $retorno");
+
+  }
+
   @override
   Widget build(BuildContext context) {
 
     //_salvar();
-    _listarUsuarios();
+    //_listarUsuarios();
+    //_excluirUsuario(9);
+    //_listarUsuarioPeloId(2);
+    //_atualizarUsuario(2);
+    //_listarUsuarioPeloId(2);
+    //_listarUsuarios();
 
     return Container();
   }
